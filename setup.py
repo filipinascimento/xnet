fc91cd94f6b24795dfd83ef8235b402be9183f08
Filipi Nascimento Silva <filipinascimento@gmail.com>
2023-10-10 22:50:27 -0400

    Added requirements.txt to MANIFEST. bumped to 1.0.3

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as f:
+    required_packages = f.read().splitlines()
+
 setuptools.setup(
     name="xnetwork",
-    version="1.0.2",
+    version="1.0.3",
     author="Filipi N. Silva and Cesar H. Comin",
     author_email="filsilva@iu.edu",
     description="Small python package to read .xnet (compleX NETwork format) files used in my other scripts.",
@@ -19,4 +22,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.0',
+    install_requires=required_packages
 )
